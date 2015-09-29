757c921cf85fb3ffc5fde64f44f91fb5e7c20171
Preston Moore <prestonkmoore@gmail.com>
2015-09-29 17:32:49 -0400

    cs now identifies the lines in the trace for open

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #define _GNU_SOURCE
 
+#ifndef PARSE_STRACE_H
+#include "parse_strace.h"
+#endif
+
 #include <string.h>
 #include <getopt.h>
 #include <stdlib.h>
@@ -18,17 +22,23 @@ int main(int argc, char** argv) {
     int opt;
     bool c_present = false;
     char* command;
-    while((opt = getopt(argc, argv, "c:")) != -1) {
+    bool t_present;
+    char* trace;
+    while((opt = getopt(argc, argv, "c:t:")) != -1) {
         switch(opt) {
             case 'c':
                 command = optarg;
                 c_present = true;
                 break;
+            case 't':
+                trace = optarg;
+                t_present = true;
+                break;
         }
     }
-    if(!(c_present)) {
+    if(!(c_present) || (!t_present)) {
         printf("Invalid required arguments\n");
-        printf("%s -c <command>\n", argv[0]);
+        printf("%s -c <command> -t <trace>\n", argv[0]);
         exit(1);
     }
     pid_t child;
@@ -43,6 +53,13 @@ int main(int argc, char** argv) {
     }
     else {
         long int orig_eax;
+        FILE* trace_file;
+        if((trace_file = fopen(trace, "r")) == NULL) {
+            printf("Failed to open trace file\n");
+            exit(1);
+        }
+        char* line = NULL;
+        size_t line_length = 0;
         while(true) {
             wait(&status);
             if(WIFEXITED(status)) {
@@ -51,7 +68,22 @@ int main(int argc, char** argv) {
             orig_eax = ptrace(PTRACE_PEEKUSER, child, 4*ORIG_EAX);
             if(orig_eax == SYS_open) {
                 if(!insyscall) {
+                    if(getline(&line, &line_length, trace_file) <= 0) {
+                        printf("Getline encountered an error or reached the end of the file");
+                        exit(1);
+                    }
+                    while(!is_open_syscall(line)) {
+                        free(line);
+                        line_length = 0;
+                        if(getline(&line, &line_length, trace_file) <= 0) {
+                            printf("Getline encountered an error or reached the end of the file");
+                            exit(1);
+                        }
+                    }
+                    printf("Corresponding line: %s", line);
                     printf("Entering: %ld\n", orig_eax);
+                    free(line);
+                    line_length = 0;
                     insyscall = true;
                 }
                 else {
