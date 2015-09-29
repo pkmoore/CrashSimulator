de3288f3b01277ece4885ab94c62aece48748ce2
Preston Moore <prestonkmoore@gmail.com>
2015-09-29 16:12:14 -0400

    made is_open_syscall work

--- parse_strace.c
+++ parse_strace.c
@@ -4,9 +4,27 @@
 
 #include <regex.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 bool is_open_syscall(char* line) {
-    return false;
+    regex_t regex;
+    if(regcomp(&regex, "open(", 0) == -1) {
+        printf("Failed to compile is_open_syscall regex");
+        exit(1);
+    }
+    int ret = regexec(&regex, line, 0, NULL, 0);
+    regfree(&regex);
+    if(ret == 0) {
+        return true;
+    }
+    else if (ret == REG_NOMATCH) {
+        return false;
+    }
+    else {
+        printf("is_open_syscall match attempt failed");
+        exit(1);
+    }
 }
 
 
