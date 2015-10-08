c1dba6e93193bda4c8374a0fd09a5a55e2a9fd39
Preston Moore <prestonkmoore@gmail.com>
2015-10-07 20:36:25 -0400

    Lazy: I moved these files and didn't do a git mv

--- python_modules/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup, Extension
-
-module1 = Extension('tracereplay', sources = ['tracereplay.c'])
-
-setup(name = 'tracereplay',
-      version = '0.1',
-      description = 'Replay a system call trace through an application',
-      ext_modules = [module1])
