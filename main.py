4e3c7b7f6bb1fe551295cd053e6821aee33947cc
Preston Moore <prestonkmoore@gmail.com>
2015-10-27 15:59:17 -0400

    Now tracking file descriptors

--- main.py
+++ main.py
@@ -12,11 +12,65 @@ import Trace
 SYS_exit = 252
 SYS_exit_group = 231
 
+FILE_DESCRIPTORS = []
+
 def next_syscall():
     s = os.wait()
     if os.WIFEXITED(s[1]):
         return False
     return True
+
+def socketcall_handler(syscall_id, syscall_object, entering):
+    subcall_handlers = {
+                        ('socket', True): socket_subcall_entry_handler,
+                        ('socket', False): socket_subcall_exit_handler,
+                        ('accept', True): accept_subcall_entry_handler,
+                        ('accept', False): accept_subcall_exit_handler
+                       }
+    try:
+        subcall_handlers[(syscall_object.name, entering)](syscall_id, syscall_object, entering)
+    except KeyError:
+        default_syscall_handler(syscall_id, syscall_object, entering)
+
+def close_entry_handler(syscall_id, syscall_object, entering):
+    pass
+
+def close_exit_handler(syscall_id, syscall_object, entering):
+    fd = syscall_object.args[0].value
+    try:
+        FILE_DESCRIPTORS.remove(fd)
+    except ValueError:
+        raise Exception('Tried to close untracked file descriptor')
+
+def socket_subcall_entry_handler(syscall_id, syscall_object, entering):
+    pass
+
+def socket_subcall_exit_handler(syscall_id, syscall_object, entering):
+    fd = syscall_object.ret
+    if fd not in FILE_DESCRIPTORS:
+        FILE_DESCRIPTORS.append(fd[0])
+    else:
+        raise Exception('Tried to store the same file descriptor twice')
+
+def open_entry_handler(syscall_id, syscall_object, entering):
+    pass
+
+def open_exit_handler(syscall_id, syscall_object, entering):
+    fd = syscall_object.ret
+    if fd not in FILE_DESCRIPTORS:
+        FILE_DESCRIPTORS.append(fd[0])
+    else:
+        raise Exception('Tried to store the same file descriptor twice')
+
+def accept_subcall_entry_handler(syscall_id, syscall_object, entering):
+    pass
+
+def accept_subcall_exit_handler(syscall_id, syscall_object, entering):
+    fd = syscall_object.ret
+    if fd not in FILE_DESCRIPTORS:
+        FILE_DESCRIPTORS.append(fd[0])
+    else:
+        raise Exception('Tried to store the same file descriptor twice')
 
 def default_syscall_handler(syscall_id, syscall_object, entering):
     print('======')
@@ -26,9 +80,16 @@ def default_syscall_handler(syscall_id, syscall_object, entering):
     print('======')
 
 def handle_syscall(syscall_id, syscall_object, entering):
-    handlers = {}
+    handlers = {
+                (102, True): socketcall_handler,
+                (102, False): socketcall_handler,
+                (6, True): close_entry_handler,
+                (6, False): close_exit_handler,
+                (5, True): open_entry_handler,
+                (5, False): open_exit_handler
+               }
     try:
-        handlers[syscall_id](syscall_id, syscall_object, entering)
+        handlers[(syscall_id, entering)](syscall_id, syscall_object, entering)
     except KeyError:
         default_syscall_handler(syscall_id, syscall_object, entering)
 
