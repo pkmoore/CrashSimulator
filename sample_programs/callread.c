74841c2e68dc83c1de274d336df000db0a69c61d
Preston Moore <prestonkmoore@gmail.com>
2015-11-01 15:27:39 -0500

    Added default buffer value to clarify testing

--- sample_programs/callread.c
+++ sample_programs/callread.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(void) {
     char buffer[8];
+    strcpy(buffer, "1234567");
     read(STDIN_FILENO, buffer, sizeof(buffer));
     buffer[7] = '\0';
     printf("%s\n", buffer);
