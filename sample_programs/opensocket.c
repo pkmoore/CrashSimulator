e20a265df1ec85bf9efab739e5f8e51cf99af65b
Preston Moore <prestonkmoore@gmail.com>
2015-12-08 17:49:15 -0500

    Simplified opensocket sample

--- sample_programs/opensocket.c
+++ sample_programs/opensocket.c
@@ -6,17 +6,11 @@
 
 int main() {
     int s = socket(AF_INET, SOCK_STREAM, 0);
-    printf("Socket returned %d\n", s);
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(6666);
-    inet_pton(AF_INET, "127.0.0.11", &addr.sin_addr);
-    int retval = bind(s, (struct sockaddr*)&addr, sizeof(struct sockaddr_in));
-    if(retval == 0) {
-        printf("success\n");
+    if(s != -1) {
+        printf("Success!\n");
     }
     else {
-        printf("Failure\n");
+        printf("Failure!\n");
     }
     return 0;
 }
