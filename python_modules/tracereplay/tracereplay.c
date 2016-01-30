ec54cfa4641b222590e399693418b1fc00159e6f
Preston Moore <prestonkmoore@gmail.com>
2016-01-30 10:30:57 -0500

    Poll now uses copy_buffer

--- python_modules/tracereplay/tracereplay.c
+++ python_modules/tracereplay/tracereplay.c
@@ -290,74 +290,29 @@ static PyObject* tracereplay_peek_address(PyObject* self, PyObject* args) {
 
 static PyObject* tracereplay_write_poll_result(PyObject* self, PyObject* args) {
     pid_t child;
-    int address;
+    void* addr;
     short fd;
     short re;
-    char buffer[sizeof(struct pollfd)];
-    struct pollfd* s = (struct pollfd*)buffer;
-    size_t writes = sizeof(struct pollfd) / sizeof(void*);
-    if(!PyArg_ParseTuple(args, "iihh", &child, &address, &fd, &re)) {
+    struct pollfd s;
+    if(!PyArg_ParseTuple(args, "iihh", &child, (int*)&addr, &fd, &re)) {
         PyErr_SetString(TraceReplayError, "write_poll_result arg parse failed");
     }
-    s->fd = fd;
-    s->events = 0;
-    s->revents = re;
-    int i = 0;
+    s.fd = fd;
+    s.events = 0;
+    s.revents = re;
     if(DEBUG) {
-        printf("E Size: %d\n", sizeof(s->events));
-        printf("FD Size: %d\n", sizeof(s->fd));
-        printf("RE Size: %d\n", sizeof(s->revents));
-        printf("C: Buffer:\n");
-        for(i = 0; i < sizeof(buffer); i++) {
-            printf("%02X ", buffer[i]);
-        }
-        printf("\n");
+        printf("E Size: %d\n", sizeof(s.events));
+        printf("FD Size: %d\n", sizeof(s.fd));
+        printf("RE Size: %d\n", sizeof(s.revents));
         printf("C: sizeof(struct pollfd) = %d\n", sizeof(struct pollfd));
-        printf("C: FD %d\n", s->fd);
-        printf("C: E %d\n", s->events);
-        printf("C: RE %d\n", s->revents);
-        printf("C: Writes %d\n", writes);
+        printf("C: FD %d\n", s.fd);
+        printf("C: E %d\n", s.events);
+        printf("C: RE %d\n", s.revents);
     }
-    int* writeptr;
-    writeptr = (int*)buffer;
-    if(DEBUG) {
-        printf("Writing: %d\n", *writeptr);
-    }
-    if(ptrace(PTRACE_POKEDATA,
-              child,
-              address,
-              *writeptr
-              ) == -1) {
-        PyErr_SetString(TraceReplayError,
-                        "Failed to poke when writing pollfd struct");
-    }
-    writeptr = (int*)(&buffer[4]);
-    if(DEBUG) {
-        printf("Writing: %d\n", *writeptr);
-    }
-    if(ptrace(PTRACE_POKEDATA,
-              child,
-              address+4,
-              *writeptr
-              ) == -1) {
-        PyErr_SetString(TraceReplayError,
-                        "Failed to poke when writing pollfd struct");
-    }
-    char tmp_buf[4];
-    int* tmp = (int*)tmp_buf;
-    if(DEBUG) {
-        printf("Values read back from child process:\n");
-        *tmp = ptrace(PTRACE_PEEKDATA, child, address, NULL);
-        for(i = 0; i < sizeof(tmp_buf); i++) {
-            printf("%02X ", tmp_buf[i]);
-        }
-        printf("\n");
-        *tmp = ptrace(PTRACE_PEEKDATA, child, address+4, NULL);
-        for(i = 0; i < sizeof(tmp_buf); i++) {
-            printf("%02X ", tmp_buf[i]);
-        }
-        printf("\n");
-    }
+    copy_buffer_into_child_process_memory(child,
+                                          addr,
+                                          (char*)&s,
+                                          sizeof(struct pollfd));
     Py_RETURN_NONE;
 }
 
