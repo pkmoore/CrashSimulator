1028b4defa7b282f3aae3178871ca894edcc1993
Preston Moore <prestonkmoore@gmail.com>
2016-02-03 23:59:38 -0500

    Fixed netcat send noconnect trace and test

--- test/test_netcat_send_noconnect.sh
+++ test/test_netcat_send_noconnect.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd .. > /dev/null;
 python main.py \
-       -c "netcat 127.0.0.1 6666" \
+       -c "netcat localhost 6666" \
        -t sample_traces/netcat_send_noconnect.strace
 echo "This is a sad unit test. If no exception it probably worked."
 cd - > /dev/null;
