a6316a9802c8c7384c4505947f0030b9feea311c
Preston Moore <prestonkmoore@gmail.com>
2016-04-07 03:20:05 +0000

    Compile callgetaddrinfo in makefile

---
+++ sample_programs/callgetaddrinfo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+int main() {
+    struct  addrinfo* ai;
+    int result = getaddrinfo("http://www.google.com", "80", NULL, &ai);
+    if(result == -1) {
+        perror("Failed: ");
+        exit(1);
+    }
+    result = getaddrinfo("http://www.toast.com", "80", NULL, &ai);
+    if(result == -1) {
+        perror("Failed: ");
+        exit(1);
+    }
+    return 0;
+}
