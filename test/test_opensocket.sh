9af6b4c693132610ebc7ed6f5f27ded6a8e6de30
Preston Moore <prestonkmoore@gmail.com>
2016-05-09 16:28:49 +0000

    Opensocket test now automatically creates trace

--- test/test_opensocket.sh
+++ test/test_opensocket.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-cd .. > /dev/null;
-output=`python main.py \
+cd ../sample_programs > /dev/null;
+strace -f -s 9999 -vvvvv -o opensocket.strace ./opensocket
+cd .. > /dev/null
+OUTPUT=$(python main.py \
        -c sample_programs/opensocket \
-       -t sample_programs/opensocket_failure.strace`
-if ! echo $output | grep -q "Failure!"
+       -t sample_programs/opensocket.strace)
+if ! echo $OUTPUT | grep -q "Success!";
     then echo "failed"
 fi
 cd - > /dev/null;
