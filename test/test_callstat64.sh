d2f4a80e8a25d78da07d5167aaaf81b9434c13b3
Preston Moore <prestonkmoore@gmail.com>
2016-05-09 16:28:13 +0000

    New traces for sample programs mean changes totest

--- test/test_callstat64.sh
+++ test/test_callstat64.sh
@@ -3,7 +3,7 @@ cd .. > /dev/null;
 output=$(python main.py \
        -c sample_programs/callstat64 \
        -t sample_programs/callstat64.strace)
-if ! echo $output | grep -q "st_ino: 11f"
+if ! echo $output | grep -q "st_ino: 73"
 then echo "$0: failed!";
 fi
 cd - > /dev/null;
--- test/test_calltcgets.sh
+++ test/test_calltcgets.sh
@@ -3,7 +3,7 @@ cd .. > /dev/null;
 output=$(python main.py \
        -c sample_programs/calltcgets \
        -t sample_programs/calltcgets.strace)
-if ! echo $output | grep -q "03 1c 7f 15 04 00 01 00 11 13 1a ff 12 0f 17 16 ff 00 00 e4 fb ff bf ec fb ff bf 1c 93 04 08 0c"
+if ! echo $output | grep -q "8a3b"
 then echo "$0: failed!";
 fi
 cd - > /dev/null;
