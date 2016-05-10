72957a0ac2cb4432432500759dc56109fae72908
Preston Moore <prestonkmoore@gmail.com>
2016-05-10 19:42:37 +0000

    Updated test to work with script

--- test/test_mtu_receiver_good.sh
+++ test/test_mtu_receiver_good.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
+# Care must be taken to send enough bytes to the listener that it meets its
+# shutdown condition. I think this is > 15 bytes. Otherwise it keeps running and
+# the test fails.
+cd ../sample_programs > /dev/null;
+# Ugly cleanup
+killall mtu_receiver_good;
+strace -f -s 9999 -vvvvv -o mtu_receiver_good.strace ./mtu_receiver_good &
+sleep 5s;
+echo "asdfawefasdf1234123412341234" | nc localhost 6666
 cd .. > /dev/null;
-python main.py \
-       -c sample_programs/mtu_receiver_good \
-       -t sample_programs/mtu_receiver_good.strace
-echo "This test does no checking! Watch for exceptions!"
-cd - > /dev/null;
+OUTPUT=$(python main.py \
+         -c sample_programs/mtu_receiver_good \
+         -t sample_programs/mtu_receiver_good.strace);
+RET=$?
+echo $OUTPUT | grep -q "asdf";
+FOUND=$?;
+rm sample_programs/mtu_receiver_good.strace;
+cd test > /dev/null;
+if [ $RET -ne 0 ] || [ $FOUND -ne 0 ];
+   then exit 1;
+fi
