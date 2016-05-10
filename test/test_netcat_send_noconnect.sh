68dd3e85eadbdb090fb11bc6b05fcdb7ef96c6b0
Preston Moore <prestonkmoore@gmail.com>
2016-05-10 21:28:22 +0000

    Updated netcat_send_noconnect test and trace

--- test/test_netcat_send_noconnect.sh
+++ test/test_netcat_send_noconnect.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+cd ../sample_traces > /dev/null;
+strace -f -s 9999 -vvvvv -o netcat_send_noconnect.strace netcat -v localhost 6666;
 cd .. > /dev/null;
-python main.py \
-       -c "netcat localhost 6666" \
-       -t sample_traces/netcat_send_noconnect.strace
-echo "This is a sad unit test. If no exception it probably worked."
-cd - > /dev/null;
+OUTPUT=$(python main.py \
+       -c "netcat -v localhost 6666" \
+       -t sample_traces/netcat_send_noconnect.strace);
+RET=$?;
+echo $OUTPUT | grep -q "Connection refused";
+FOUND=$?;
+cd test > /dev/null;
+if [ $RET -ne 0 ] || [ $FOUND -ne 0 ];
+   then exit 1;
+fi
