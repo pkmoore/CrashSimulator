2191012786ece6c540755c2f551948012147bd5f
Preston Moore <prestonkmoore@gmail.com>
2016-05-13 18:33:21 +0000

    Prettier output for test.sh

--- test/test.sh
+++ test/test.sh
@@ -6,11 +6,13 @@ if [ -f .tests.log ];
 fi
 touch .tests.log
 for file in test_*.sh; do
+    echo "////////// $file //////////"
     ./$file;
     if [ $? -ne 0 ];
         then echo "$file failed!" >> .tests.log;
         RESULTS=$((RESULTS + 1))
     fi
+    echo "///////////////////////////"
 done
 echo "//////////// RESULTS /////////////";
 if [ $RESULTS -ne 0 ];
