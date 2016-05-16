0c4c3e10884fbedd7091d7170a30c8d355e36899
Preston Moore <prestonkmoore@gmail.com>
2016-05-16 20:49:25 +0000

    Redirect stderr to stdout in netcat_noconnect test

--- test/test_netcat_send_noconnect.sh
+++ test/test_netcat_send_noconnect.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+
+# This script redirects STDERR into STDOUT for testing purposes. This has been
+# shown to cause issues in other cases. It seems fine here but might cause
+# problems in the future.
+
+
 cd ../sample_traces > /dev/null;
 strace -f -s 9999 -vvvvv -o netcat_send_noconnect.strace netcat -v localhost 6666;
 cd .. > /dev/null;
 OUTPUT=$(python main.py \
        -c "netcat -v localhost 6666" \
-       -t sample_traces/netcat_send_noconnect.strace);
+       -t sample_traces/netcat_send_noconnect.strace 2>&1);
 RET=$?;
 echo $OUTPUT | grep -q "Connection refused";
 FOUND=$?;
