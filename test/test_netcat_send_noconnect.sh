d6396c18c8a0cab5e45a8749223cd9819ac0564b
Preston Moore <prestonkmoore@gmail.com>
2016-05-25 14:01:41 +0000

    netcat_send_noconnect test preserves sample trace

--- test/test_netcat_send_noconnect.sh
+++ test/test_netcat_send_noconnect.sh
@@ -5,15 +5,15 @@
 # problems in the future.
 
 
-cd ../sample_traces > /dev/null;
+cd ..> /dev/null;
 strace -f -s 9999 -vvvvv -o netcat_send_noconnect.strace netcat -v localhost 6666;
-cd .. > /dev/null;
 OUTPUT=$(python main.py \
        -c "netcat -v localhost 6666" \
-       -t sample_traces/netcat_send_noconnect.strace 2>&1);
+       -t netcat_send_noconnect.strace 2>&1);
 RET=$?;
 echo $OUTPUT | grep -q "Connection refused";
 FOUND=$?;
+rm netcat_send_noconnect.strace;
 cd test > /dev/null;
 if [ $RET -ne 0 ] || [ $FOUND -ne 0 ];
    then exit 1;
