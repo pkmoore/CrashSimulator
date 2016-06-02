0a2fa3e9b0bd6054f4dc4cab70c40257bb173d9c
Preston Moore <prestonkmoore@gmail.com>
2016-06-02 02:40:11 +0000

    netcat receive test

--- test/test_netcat_receive.sh
+++ test/test_netcat_receive.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 cd .. > /dev/null;
-python main.py \
-       -c "nc -l 6666" \
-       -t sample_traces/netcat_receive.strace
-echo "This test does no checking! Watch for exceptions!"
-cd - > /dev/null;
+#strace -f -s 9999 -vvvvv -o netcat_receive.strace netcat -v -l 6666 &
+#sleep 5s;
+echo "asdfasdf" | netcat localhost 6666;
+OUTPUT=$(python main.py \
+         -c "netcat -v -l 6666" \
+         -t sample_traces/netcat_receive.strace 2>&1);
+RET=$?;
+echo $OUTPUT | grep -q "asdfasdf";
+FOUND=$?;
+#rm netcat_receive.strace;
+cd test > /dev/null;
+if [ $RET -ne 0 ] || [ $FOUND -ne 0 ];
+   then exit 1;
+fi
