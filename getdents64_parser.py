b8297787b100c000a3a568b4332f2e75200f7bf0
Preston Moore <prestonkmoore@gmail.com>
2016-11-15 07:29:26 +0000

    getdents64 handler supports more file types

--- getdents64_parser.py
+++ getdents64_parser.py
@@ -1,6 +1,13 @@
 DIRENT_TYPES = {
-    'DT_DIR': 8,
-    'DT_REG': 4
+    'DT_UNKNOWN': 0,
+    'DT_FIFO': 1,
+    'DT_CHR': 2,
+    'DT_DIR': 4,
+    'DT_BLK': 6,
+    'DT_REG': 8,
+    'DT_LNK': 10,
+    'DT_SOCK': 12,
+    'DT_WHT': 14,
 }
 
 
