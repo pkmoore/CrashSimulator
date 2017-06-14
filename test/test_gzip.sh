ea90eea30d29270ed2a4d1e556fc4647db5aa367
Ryan <rjp2@williams.edu>
2017-06-14 14:36:57 -0400

    test_gzip changed so c output will appear

--- test/test_gzip.sh
+++ test/test_gzip.sh
@@ -1,28 +1,10 @@
 #!/bin/sh
 cd ../sample_programs > /dev/null;
-#tmpfile="$(mk zippee.txt)";
-#trace_name=gzip.strace;
-#command="\"['gzip', 'sample_programs/zippee.txt']\"";
-#xecho $command
-
 mk zippee.txt;
-strace -f -s 9999 -vvvvv -o gzip.strace gzip zippee.txt;
-gzip -d zippee.txt;
 cd .. > /dev/null;
 
-OUTPUT=$(python main.py \
-	 -c "['gzip', 'sample_programs/zippee.txt']" \
-	 -t sample_programs/gzip.strace \
-	 -l DEBUG);
+#strace -f -s 9999 -vvvvv -o sample_programs/gzip.strace gzip sample_programs/zippee.txt;
+gzip -d sample_programs/zippee.txt;
+python main.py -c "['gzip', 'sample_programs/zippee.txt']" -t sample_programs/gzip.strace -l DEBUG
 
-RET=$?
-$OUTPUT;# && $(find sample_programs/zippee.txt.gz | wc -l);
-#echo $OUTPUT | grep -q "test.{6,}"
 
-FOUND=$?
-0;#rm sample_programs/gzip.strace;# && rm sample_programs/zippee.txt;#.gz;
-
-cd test > /dev/null;
-if [ $RET -ne 0 ] || [ $FOUND -ne 0 ];
-   then exit 1
-fi
