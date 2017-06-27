40067b0f4a1f6f3e98d0a40a25ba155a31a0ebf9
Ryan <rjp2@williams.edu>
2017-06-27 15:51:49 -0400

    further whitespace fixes

--- file_handlers.py
+++ file_handlers.py
@@ -421,7 +421,7 @@ def write_entry_handler(syscall_id, syscall_object, pid):
     bytes_from_trace = bytes_from_trace.decode('string-escape')
     logging.debug(bytes_from_trace.encode('hex'))
     logging.debug(bytes_from_execution.encode('hex'))
-    #if bytes_from_trace != bytes_from_execution:
+    # if bytes_from_trace != bytes_from_execution:
     #    raise ReplayDeltaError('Bytes from trace don\'t match bytes from '
     #                           'execution!')
     fd = int(syscall_object.args[0].value)
